6bf4a6cfcaf3782ed5521282f99872db65615d46
robmainah <robmainah@gmail.com>
2023-02-28 14:53:12 +0300

    set default hosts

--- project_settings/settings.py
+++ project_settings/settings.py
@@ -30,8 +30,12 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+# Add custom ip addresses
 # add alowable hosts without spaces "localhost,127.0.0.1"
-ALLOWED_HOSTS = env('ALLOWED_HOSTS').split(',')
+try:
+    ALLOWED_HOSTS = env('ALLOWED_HOSTS').split(',')
+except:
+    ALLOWED_HOSTS = []
 
 
 # Application definition
